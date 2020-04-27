aec03fedb019f82d31e2f6603ed3021964175b47
ankitgoyal0301 <goyalankit3129@gmail.com>
2020-04-27 17:23:11 +0530

    URL Manager commit

---
+++ popup.js
@@ -0,0 +1,36 @@
+$(function () {
+  chrome.storage.sync.get(["total", "limit"], function (budget) {
+    $("#total").text(budget.total);
+    $("#limit").text(budget.limit);
+  });
+
+  $("#spendAmount").click(function () {
+    chrome.storage.sync.get(["total", "limit"], function (budget) {
+      var newTotal = 0;
+      if (budget.total) {
+        newTotal += parseInt(budget.total);
+      }
+
+      var amount = $("#amount").val();
+      if (amount) {
+        newTotal += parseInt(amount);
+      }
+
+      chrome.storage.sync.set({ total: newTotal }, function () {
+        if (amount && newTotal >= budget.limit) {
+          var notifOptions = {
+            type: "basic",
+            iconUrl: "icon48.png",
+            title: "limit reached!",
+            message: "Uh oh! Looks like you have reached your limit!",
+          };
+          chrome.notifications.create("limitNotif", notifOptions);
+          chrome.notifications.clear("limitNotif");
+        }
+      });
+
+      $("#total").text(newTotal);
+      $("#amount").val("");
+    });
+  });
+});
